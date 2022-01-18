42e32a623c5c998fd0c9846cc71c6aa4594b828c
sangy1 <56744256+sangy1@users.noreply.github.com>
2022-01-17 20:11:30 -0500

    Prototype
    Added the footer CSS is not applied yet and not fully copmlete

---
+++ covid_countdown/src/components/header_footer/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/* Column 1 */}
+                    <div className="col">
+                        <ul classname="list-unstyled">
+                            <li>Find us at:</li>
+                            <li>Address</li>
+                            <li>123 Main Street</li>
+                            <li>New York, NY 10001</li>
+                        </ul>
+                    </div>
+                    {/* Column 2 */}
+                    <div className="col">
+                        <ul classname="list-unstyled">
+                            <li>Website Name</li>
+                        </ul>
+                    </div>
+                    {/* Column 3 */}
+                
+                    <div className="col">
+                        <input type="email" placeholder="Email Address" ></input> 
+                        <button type="submit" value="submit">Submit</button>
+                        <p>Content Disclaimer: This is a fake website plase dont believe everything in here. 
+                            Blah blah blah, we don’t actually believe covid 19 is fake in anyway. but the image 
+                            looked good so its the footer. Also, please don’t sue us for using the images</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
